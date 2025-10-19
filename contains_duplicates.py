c5eb0421d2e43493b3a1a8b2c81940fb0ac578df
Asmita Chhabra <asmitachhabra04@gmail.com>
2025-10-19 09:55:03 +0530

    Implement containsDuplicate method in Solution class

---
+++ contains_duplicates.py
@@ -0,0 +1,9 @@
+
+# Code
+```python3 []
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_nums = set(nums)
+        return len(unique_nums) != len(nums)
+
+```
